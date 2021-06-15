c03272d978c6a90dd4e8cea59dc691bd52f36de7
marcosihe <marcosherrera.ib@gmail.com>
2021-06-15 00:54:20 -0300

    Se argego el tp obligatorio

---
+++ TP_Nº0_Herrera_Marcos/src/agendatelefonica/Main.java
@@ -0,0 +1,18 @@
+package agendatelefonica;
+
+import IGU.Agenda;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Agenda ppal = new Agenda();
+        ppal.setVisible(true);
+        ppal.setLocationRelativeTo(null);
+       
+    }
+    
+}
